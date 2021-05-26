4cdc47b8370c66c520698fd7e5e840b02fbd4020
kaiop <kaiop@LAPTOP-BB30E84V>
2021-05-26 16:06:02 -0300

    Iniciando projeto

---
+++ src/main/java/Objetos/Fornecedor.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Objetos;
+
+/**
+ *
+ * @author kaiop
+ */
+public class Fornecedor {
+
+    private String cnpj;
+    private String nomeRepresentante;
+    private String razaoSocial;
+    private String endereco;
+    private String telefone;
+
+    /**
+     * @return the cnpj
+     */
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    /**
+     * @param cnpj the cnpj to set
+     */
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    /**
+     * @return the nomeRepresentante
+     */
+    public String getNomeRepresentante() {
+        return nomeRepresentante;
+    }
+
+    /**
+     * @param nomeRepresentante the nomeRepresentante to set
+     */
+    public void setNomeRepresentante(String nomeRepresentante) {
+        this.nomeRepresentante = nomeRepresentante;
+    }
+
+    /**
+     * @return the razaoSocial
+     */
+    public String getRazaoSocial() {
+        return razaoSocial;
+    }
+
+    /**
+     * @param razaoSocial the razaoSocial to set
+     */
+    public void setRazaoSocial(String razaoSocial) {
+        this.razaoSocial = razaoSocial;
+    }
+
+    /**
+     * @return the endereco
+     */
+    public String getEndereco() {
+        return endereco;
+    }
+
+    /**
+     * @param endereco the endereco to set
+     */
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    /**
+     * @return the telefone
+     */
+    public String getTelefone() {
+        return telefone;
+    }
+
+    /**
+     * @param telefone the telefone to set
+     */
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+}
---
+++ src/main/java/Objetos/Produto.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Objetos;
+
+/**
+ *
+ * @author kaiop
+ */
+public class Produto {
+    
+    private String nome;
+    private String descricao;
+    private String fornecedor;
+    private int quantidade;
+    private double valorTotal;
+    private double valorUnitario;
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the descricao
+     */
+    public String getDescricao() {
+        return descricao;
+    }
+
+    /**
+     * @param descricao the descricao to set
+     */
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    /**
+     * @return the fornecedor
+     */
+    public String getFornecedor() {
+        return fornecedor;
+    }
+
+    /**
+     * @param fornecedor the fornecedor to set
+     */
+    public void setFornecedor(String fornecedor) {
+        this.fornecedor = fornecedor;
+    }
+
+    /**
+     * @return the quantidade
+     */
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    /**
+     * @param quantidade the quantidade to set
+     */
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    /**
+     * @return the valorTotal
+     */
+    public double getValorTotal() {
+        return valorTotal;
+    }
+
+    /**
+     * @param valorTotal the valorTotal to set
+     */
+    public void setValorTotal(double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    /**
+     * @return the valorUnitario
+     */
+    public double getValorUnitario() {
+        return valorUnitario;
+    }
+
+    /**
+     * @param valorUnitario the valorUnitario to set
+     */
+    public void setValorUnitario(double valorUnitario) {
+        this.valorUnitario = valorUnitario;
+    }
+    
+}
